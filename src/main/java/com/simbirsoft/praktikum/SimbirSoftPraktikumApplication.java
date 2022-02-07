00de6f4a44e59c1f17109b4bb13d305419061db0
shodkvest <shodkvest@gmail.com>
2022-02-07 13:53:09 +0600

    Add home controller

---
+++ src/main/java/com/simbirsoft/praktikum/SimbirSoftPraktikumApplication.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.praktikum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirSoftPraktikumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimbirSoftPraktikumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simbirsoft/praktikum/SimbirSoftPraktikumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.praktikum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbirSoftPraktikumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
