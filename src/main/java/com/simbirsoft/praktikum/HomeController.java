5715937fa0e03eec23bf97e4c83a18c86af4f096
shodkvest <shodkvest@gmail.com>
2022-02-07 18:03:14 +0600

    Add jsoup

--- src/main/java/com/simbirsoft/praktikum/HomeController.java
+++ src/main/java/com/simbirsoft/praktikum/HomeController.java
@@ -1,15 +1,32 @@
 package com.simbirsoft.praktikum;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("https://en.wikipedia.org/").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (doc != null) {
+            System.out.println(doc.body());
+            model.addAttribute("docbody", doc.body());
+        }
+
         return "index";
     }
 }
